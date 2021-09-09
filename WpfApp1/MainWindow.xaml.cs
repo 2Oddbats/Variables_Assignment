c1e8689095751dc660095757b8f1152cd0608a37
William Skelton <22wiskelton@macon.k12.mo.us>
2021-09-09 12:48:40 -0500

    Learning to use variables like textboxes

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonMyMessage_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName;
+
+            string lastName;
+
+            firstName = textBoxFirstName.Text;
+
+            lastName = textBoxLastName.Text;
+
+            labelMyMessage.Content = "Hello " + firstName + " " + lastName + ", good to see you!";
+
+
+        }
+
+        private void buttonClearMe_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxFirstName.Text = "";
+
+            textBoxLastName.Text = "";
+
+            labelMyMessage.Content = "";
+
+
+        }
+    }
+}
